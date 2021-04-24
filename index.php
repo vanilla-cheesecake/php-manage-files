9bb97fd5a39085176f115bc2f8bb995572c07d8e
golez <golez.sf@gmail.com>
2021-04-25 04:08:42 +0800

    notepad as a database

--- index.php
+++ index.php
@@ -2,17 +2,22 @@
 <html lang="en">
 
 <head>
-    <title>Student Info</title>
+    <title>Student Records</title>
     <style>
+        body {
+            font: 15px Arial, sans-serif;
+
+        }
+
         table {
-            width: 500px;
+            width: 700px;
         }
 
         table,
         th,
         td {
             border: 1px solid black;
-            text-align: left;
+            text-align: center;
             border-collapse: collapse;
 
         }
@@ -22,54 +27,47 @@
             padding: 10px;
         }
     </style>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script>
-
-    </script>
-
 </head>
 
 <body>
-
-    <script>
-        function deleteRow() {
-            document.querySelectorAll('#table .select:checked').forEach(e => {
-                e.parentNode.parentNode.remove()
-            });
-        }
-    </script>
-
     <?php
     error_reporting(0);
 
+    // DELETE OPERATION 
     if (isset($_POST['submit'])) {
+        // GET VALUE FROM CHECKBOXES
         $student = $_POST['student'];
+        // ENABLE MULTIPLE CHECKBOX SELECTION
         foreach ($student as $key => $Values) {
+            // READ ALL STRING IN THE FILE FROM THE TEXT DATABASE 
             $contents = file_get_contents('students.txt');
+            // REPLACE SELECTED VALUES
             $contents = str_replace($Values, '', $contents);
+            // SAVE REPLACED FILES
             file_put_contents('students.txt', $contents);
         }
-        echo "<script>alert();</script>";
+        echo "<script>alert(\"Student(s) successfully deleted from text file storage\");</script>";
     } else {
         error_reporting(0);
     }
-
     ?>
-
-
     <center>
-        <h1>STUDENT INFORMATION</h1>
-        <h3>MULTIPLE DELETE SELECTION USING FILES</h3>
+        <h3>PHP MULTIPLE DELETE SELECTION USING FILES<br />
+            <span>Textfile as a record storage</span>
+        </h3>
+        <h1>STUDENT RECORDS</h1>
+
         <form action="" method="POST" name="myform">
             <table id="table">
                 <tr>
                     <th>Select</th>
                     <th>ID</th>
                     <th>Name</th>
+                    <th>Lastname</th>
                     <th>Email</th>
                 </tr>
                 <?php
-                // OPEN FILE STORAGE
+                // OPEN THE TEXT FILE STORAGE
                 $file = fopen('students.txt', 'r');
                 // GET DATA FOR EACH LINE
                 while ($line = fgets($file)) {
@@ -79,24 +77,26 @@
                     $studentInfo = implode($array, "|");
                     // Convert line into array 
                     $studentInfoArray = explode("|", $studentInfo);
+                    // ASSIGN VALUES TO EACH VARIABLE
                     $studentID = $studentInfoArray[0];
                     $studentName = $studentInfoArray[1];
-                    $studentEmail = $studentInfoArray[2];
+                    $studentLastName = $studentInfoArray[2];
+                    $studentEmail = $studentInfoArray[3];
 
                     // DISPLAY EACH STUDENT DATA INTO THE TABLE
-                    echo ("<tr> <td><input type=\"checkbox\" value=\"$studentID|$studentName|$studentEmail\" name=\"student[]\" class=\"select\"></td>
+                    echo ("<tr> <td><input type=\"checkbox\" value=\"$studentID|$studentName|$studentLastName|$studentEmail\" name=\"student[]\" class=\"select\"></td>
                         <td>" . $studentID . "</td>
                         <td>" . $studentName . "</td>
+                        <td>" . $studentLastName . "</td>
                         <td>" . $studentEmail . "</td>
                         </tr>");
                 }
-
-
                 // CLOSE THE FILE STORAGE
                 fclose($file);
                 ?>
             </table>
-            <input type="submit" name="submit" value="submit">
+            <br>
+            <input type="submit" name="submit" value="Delete Student">
         </form>
         <br>
     </center>
